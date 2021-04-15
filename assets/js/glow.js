72cbad211417c8b31e20097d5c5264b91bc8c04f
LouiseSteward <louisesteward@gmail.com>
2021-04-15 14:39:53 +0200

    Add JS to move elements below the lower glow

--- assets/js/glow.js
+++ assets/js/glow.js
@@ -1,11 +1,10 @@
-function ebAddGlow (selector) {
-    "use strict";
+function ebAddGlowToElement (selector) {
 
     var nodesToGlow = document.querySelectorAll(selector);
 
     nodesToGlow.forEach(function (node) {
-        // give it an event listener, which adds a class of css-glow when it 
-        // is being hovered over, or touched on a touchscreen
+        // give it an event listener, which adds a class of css-glow when it
+        // is being hovered over
         node.addEventListener("mouseenter", function () {
             node.classList.add("css-glow");
         })
@@ -15,6 +14,22 @@ function ebAddGlow (selector) {
     })
 }
 
-ebAddGlow("ul.choice-options li");
-ebAddGlow(".part-page-button");
-ebAddGlow(".button");
+ebAddGlowToElement("ul.choice-options li");
+ebAddGlowToElement(".part-page-button");
+ebAddGlowToElement(".button");
+
+function ebMoveElementBelowGlow (selector) {
+    // some elements need to live outside .main-content so that they
+    // are below the footer glow
+
+    var nodeToMove = document.querySelector(selector);
+    var mainContentNode = document.querySelector(".main-content");
+
+    if (nodeToMove) {
+        mainContentNode.after(nodeToMove);
+    }
+
+}
+
+ebMoveElementBelowGlow(".call-to-action");
+ebMoveElementBelowGlow(".infobox");
