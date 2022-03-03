caebb900a816c4539b66831700e081dd635307b0
Arthur Attwell <arthur@arthurattwell.com>
2022-03-03 17:56:28 +0200

    Add space before auto page numbers

--- assets/js/page-numbers.js
+++ assets/js/page-numbers.js
@@ -7,7 +7,7 @@ function ebAddPageNumbers() {
     if (typeof Prince.addScriptFunc === "function") {
 
         Prince.addScriptFunc("turnToPage", function (targetPage) {
-            return " Turn to page " + targetPage + ".";
+            return "\u00A0" + " Turn to page " + targetPage + ".";
         });
 
         Prince.addScriptFunc("pagereference", function (currentPage, targetPage) {
@@ -16,7 +16,7 @@ function ebAddPageNumbers() {
                 return "";
             }
             // otherwise show a space and the page number in parentheses
-            return "(Page " + targetPage + ")";
+            return "\u00A0" + "(page " + targetPage + ")";
         });
         Prince.addScriptFunc("pagereferencePlain", function (currentPage, targetPage) {
             // if the target is on this page, return blank
