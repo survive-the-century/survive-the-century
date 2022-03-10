28382e5b6a1bc5903242739649d75d71df12d551
Arthur Attwell <arthur@arthurattwell.com>
2022-03-10 17:46:31 +0200

    Add not-the-last-page indicator

--- assets/js/page-numbers.js
+++ assets/js/page-numbers.js
@@ -1,5 +1,5 @@
 /*jslint */
-/*globals Prince */
+/*globals Prince, document */
 
 function ebAddPageNumbers() {
     "use strict";
@@ -26,7 +26,45 @@ function ebAddPageNumbers() {
             // otherwise show a space and the page number
             return "\u00A0" + targetPage;
         });
+        Prince.addScriptFunc("lastPageCheck", function (currentPage, targetPage) {
+            // if the target is on this page, or a string zero
+            // (i.e. not a valid page for Prince) return blank
+            if (currentPage === targetPage
+                    || targetPage === '0') {
+                return "";
+            }
+            // otherwise show an arrow
+            return "\u2192";
+        });
     }
 }
 
+// Add a not-the-last-page arrow in the right-bottom margin
+function lastPageCheck() {
+    'use strict';
+
+    // Create a target at the end of the doc,
+    // which will only appear on its last page.
+    // It must be block-level element (not a span)
+    // for Prince to include it on the page as a valid target.
+    var endContentTarget = document.createElement('div');
+    endContentTarget.id = 'end-content';
+    var wrapper = document.getElementById('wrapper');
+    wrapper.appendChild(endContentTarget);
+
+    // Add content to the margin-box. The content is created in
+    // ebAddPageNumbers() above as lastPageCheck.
+    // It's then placed into the margin box here.
+    // We do this with an on-page <style> element because
+    // the url('#end-content') resolves relative to the file
+    // the CSS is in, so if we do this in the main, external CSS file,
+    // e.g. print-pdf.scss, the URL will resolve to print-pdf.scss#end-content,
+    // rather than [filename].html#end-content. For more detail see
+    // https://www.princexml.com/forum/topic/4353/target-counter-not-working
+    var lastPageStyleElement = document.createElement('style');
+    lastPageStyleElement.innerHTML = '@page { @right-bottom { content: prince-script(lastPageCheck, counter(page), target-counter(url("#end-content"), page));} }';
+    document.head.appendChild(lastPageStyleElement);
+}
+
 ebAddPageNumbers();
+lastPageCheck();
